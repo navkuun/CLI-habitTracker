6010d370b7b163e505c5b3cdb1b11489224da393
ali ali <navkhan5555@gmail.com>
2021-07-18 12:46:41 +0100

    f:

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from cli_tables.cli_tables import * 
 import time
 import re
-import json
+import csv 
 import os
 
 # Create new table
@@ -11,24 +11,36 @@ def create_new_table():
     print("- You have chosen to create a new table -")
     table_file_name = input("Table name : ")
     OUTPUT_DIR = os.getcwd()
-    f = open(os.path.join(OUTPUT_DIR, f'{table_file_name}.json'), 'w')
-    f.close()
+    f_path = open(os.path.join(OUTPUT_DIR, f'{table_file_name}.csv'), 'w')
+    f_path.close()
     width = int(input("How many days do you want to track use this table ? :"))
     string_habits = input("Please enter a list of habits, seperated by a comma : ")
     list_habits =  string_habits.split(',')
-    # habits_store = {}
-    # for habit in list_habits:
-    #     for i in range(1,width+1):
-    #         habits_store[habit][i] = ' '
 
-    fields = ['Habits']
-    records = [[]] 
-    for habit in list_habits:
-        records.append(habit)
-    for i in range(1,width+1):
-        fields.append(i)
+    # creating the header - can be thought of as the fields 
+    header = ['Habits']
+    for i in range(1, width):
+        header.append(f"Day {i}")
 
-    print_table(fields, records)
+    # creating records
+    data = []
+    for habit in list_habits: 
+        data.append([habit])
+
+    for i in range(len(list_habits)):
+        for j in range(width):
+            data[i].append(' ')
+
+    # writing data and header to path 
+    string_full_path = os.path.join(OUTPUT_DIR, f'{table_file_name}.csv')
+    with open(string_full_path, 'w') as work:
+        z = csv.writer(work)
+        z.writerow(header)
+        z.writerows(data)
+
+    print(f"- creation of the {table_file_name} table successful-")
+    
+
     
 # Delete a table
 def delete_table():
@@ -39,11 +51,33 @@ def delete_table():
     directories = os.listdir(path) # list of file names
 
     for file in directories:
-        match = re.search("\.json$", file)
+        match = re.search("\.csv$", file)
         if match:                
             full_path = os.path.join(path, file)
             os.remove(full_path)
 
+def display_table():
+    print("- you have chosen to display a table -") 
+    table_name = input("Please enter your table name : ")
+
+    print("You have choose multiple views :") 
+    print("For GUI(pretty) view select: 1")
+    print("For CLI(normal) view select: 2")
+    choice = int(input())
+    if choice == 1: 
+        pass  # to do 
+    else:
+        try:
+            file_csv = open(f"{table_name}.csv")
+        except:
+            print("error - no table of that name found ")
+            pass
+
+        data_csv = csv.reader(file_csv)
+        list_csv = list(data_csv)
+        print(list_csv)
+
+    
 
 # main 
 def main():
@@ -67,8 +101,7 @@ def main():
         elif choice == 2:
             delete_table()
         elif choice == 3:
-           print(3) 
-
+            display_table()
         want_to_continue = input("Continue [Y/n] ")
 
 
