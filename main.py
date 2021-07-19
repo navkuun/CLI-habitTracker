8f1e559bc203b962019f6e38aa7daf087291cef6
ali ali <navkhan5555@gmail.com>
2021-07-19 21:38:09 +0100

    finished CLI-table display

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # imports
-from cli_tables.cli_tables import * 
+from cli_tables.cli_tables import *
 import time
 import re
-import csv 
+import csv
 import os
 
 # Create new table
+
+
 def create_new_table():
     time.sleep(0.1)
     print("- You have chosen to create a new table -")
@@ -14,24 +16,25 @@ def create_new_table():
     f_path = open(os.path.join(OUTPUT_DIR, f'{table_file_name}.csv'), 'w')
     f_path.close()
     width = int(input("How many days do you want to track use this table ? :"))
-    string_habits = input("Please enter a list of habits, seperated by a comma : ")
-    list_habits =  string_habits.split(',')
+    string_habits = input(
+        "Please enter a list of habits, seperated by a comma : ")
+    list_habits = string_habits.split(',')
 
-    # creating the header - can be thought of as the fields 
+    # creating the header - can be thought of as the fields
     header = ['Habits']
-    for i in range(1, width):
+    for i in range(1, width+1):
         header.append(f"Day {i}")
 
     # creating records
     data = []
-    for habit in list_habits: 
+    for habit in list_habits:
         data.append([habit])
 
     for i in range(len(list_habits)):
         for j in range(width):
             data[i].append(' ')
 
-    # writing data and header to path 
+    # writing data and header to path
     string_full_path = os.path.join(OUTPUT_DIR, f'{table_file_name}.csv')
     with open(string_full_path, 'w') as work:
         z = csv.writer(work)
@@ -39,33 +42,35 @@ def create_new_table():
         z.writerows(data)
 
     print(f"- creation of the {table_file_name} table successful-")
-    
 
-    
+
 # Delete a table
 def delete_table():
     time.sleep(0.1)
     print("- You have chosen to delete a table -")
     table_file_name = input("Enter table name, to remove : ")
     path = os.getcwd()
-    directories = os.listdir(path) # list of file names
+    directories = os.listdir(path)  # list of file names
 
     for file in directories:
         match = re.search("\.csv$", file)
-        if match:                
+        if match:
             full_path = os.path.join(path, file)
             os.remove(full_path)
 
-def display_table():
-    print("- you have chosen to display a table -") 
+
+def display_table():  # Currently working on
+
+    print("- you have chosen to display a table -")
     table_name = input("Please enter your table name : ")
 
-    print("You have choose multiple views :") 
+    print("You have choose multiple views :")
     print("For GUI(pretty) view select: 1")
     print("For CLI(normal) view select: 2")
+
     choice = int(input())
-    if choice == 1: 
-        pass  # to do 
+    if choice == 1:
+        pass  # to do
     else:
         try:
             file_csv = open(f"{table_name}.csv")
@@ -75,11 +80,12 @@ def display_table():
 
         data_csv = csv.reader(file_csv)
         list_csv = list(data_csv)
-        print(list_csv)
 
-    
+        table_list = [x for x in list_csv if x]
+        print_table(table_list, double_hline=True, double_vline=True)
 
-# main 
+
+# main
 def main():
     print("Let's get started, welcome to the habit tracker")
     time.sleep(0.5)
@@ -97,7 +103,7 @@ def main():
             print("Error - no 'valid' choice chosen")
 
         if choice == 1:
-            create_new_table()            
+            create_new_table()
         elif choice == 2:
             delete_table()
         elif choice == 3:
